6e85f0344be876442a04710cdd0bede5c5381f8e
Harnau <harnau@omatech.com>
2019-07-26 14:26:22 +0200

    button add relations font-size

--- src/admin/views/templates/instances_list.blade.php
+++ src/admin/views/templates/instances_list.blade.php
@@ -2,7 +2,7 @@
     @php($id_rel=0)
     <form class="form" id="relation_all" name="relation_all" method="post" enctype="multipart/form-data" action="{{route('editora.action', 'join_all')}}">
         {{ csrf_field() }}
-        <button class="btn clr-secondary">{{getMessage('info_word_join')}}</button>
+        <button class="btn clr-secondary" style="font-size:14px">{{getMessage('info_word_join')}}</button>
         <input type="hidden" name="p_pagina" value="1"/>
         <input type="hidden" name="p_class_id" value="{{$parent['class_id']}}"/>
         <input type="hidden" name="p_relation_id" value="{{$parent['rel_id']}}"/>
