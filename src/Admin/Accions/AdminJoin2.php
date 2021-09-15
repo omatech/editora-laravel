9f42dd2208416719b3830b25345d65e0590153ed
Hèctor Arnau <harnau@omatech.com>
2021-09-15 15:37:22 +0200

    :fis bug add relation

--- src/Admin/Accions/AdminJoin2.php
+++ src/Admin/Accions/AdminJoin2.php
@@ -14,23 +14,22 @@ class AdminJoin2 extends AuthController
         $security = new Security;
         $params = get_params_info();
         $parent_class_id = $parent_id = null;
-        if (Session::get('rol_id')==1 || $security->getAccess('browseable',$params)) {
-            $instances = new Instances;
-            $re=new relations();
 
-            $params['p_mode'] = 'R';
-            $params['p_acces_type']='A';
+        $instances = new Instances;
+        $re = new relations();
 
-            $parent_class_id = $params['param1']=$params['param10'];
-            $parent_id = $params['param2']=$params['param11'];
+        $params['p_mode'] = 'R';
+        $params['p_acces_type'] = 'A';
+
+        $parent_class_id = $params['param1'] = $params['param10'];
+        $parent_id = $params['param2'] = $params['param11'];
 
 
-            $re->createRelation($params);
+        $re->createRelation($params);
 
-            $params['p_mode'] = $p_mode = 'V';
-            $instances->logAccess($params);
-        }
+        $params['p_mode'] = $p_mode = 'V';
+        $instances->logAccess($params);
 
-        return redirect(route('editora.action', 'view_instance?p_pagina=1&p_class_id='.$parent_class_id.'&p_inst_id='.$parent_id));
+        return redirect(route('editora.action', 'view_instance?p_pagina=1&p_class_id=' . $parent_class_id . '&p_inst_id=' . $parent_id));
     }
 }
