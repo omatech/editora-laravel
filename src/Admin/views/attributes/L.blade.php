7256c1817c8ac0070a60ac8ec81c6483c94076c9
Agusti Pons <apons@omatech.com>
2021-11-16 08:43:39 +0100

    bug in L blade

--- src/Admin/views/attributes/L.blade.php
+++ src/Admin/views/attributes/L.blade.php
@@ -1,9 +1,18 @@
 {{-- Lookup --}}
 @if($p_mode=='V')
+  @php
+    $default_id=$attribute['lookup_info']['info']['default_id'];
+    $lookup_value=$attribute['params']->lookup->$default_id[0];  
+    if (isset($attribute['lookup_info']['selected_values'][0]['label']))
+    {
+        $lookup_value=$attribute['lookup_info']['selected_values'][0]['label'];
+    }  
+  @endphp
+
     <div class="column column-text">
         <div class="form-group">
             <label for="{{$attribute_name}}" class="form-label">{{$attribute['caption']}} {!! _attributeInfo($attribute['id'], $attribute['name'], $attribute['type']) !!}</label>
-            <input type="text" class="form-control" name="{{$attribute_name}}" value="{{$attribute['lookup_info']['selected_values'][0]['label']}}" disabled="disabled">
+            <input type="text" class="form-control" name="{{$attribute_name}}" value="{{$lookup_value}}" disabled="disabled">
         </div>
     </div>
 @elseif($p_mode=='U' || $p_mode=='I')
