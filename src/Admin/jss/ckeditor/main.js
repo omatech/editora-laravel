94837226acffda406ecd937b6a890cb136534cb8
Christian Bohollo <cbohollo@omatech.com>
2024-11-13 15:29:10 +0100

    ckeditor

--- src/Admin/jss/ckeditor/main.js
+++ src/Admin/jss/ckeditor/main.js
@@ -1,15 +1,75 @@
 import {
 	ClassicEditor,
 	AccessibilityHelp,
+	Alignment,
+	Autoformat,
+	AutoImage,
 	AutoLink,
 	Autosave,
 	BalloonToolbar,
+	BlockQuote,
 	Bold,
+	Code,
+	CodeBlock,
 	Essentials,
+	FindAndReplace,
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+	FullPage,
+	GeneralHtmlSupport,
+	Heading,
+	Highlight,
+	HorizontalLine,
+	HtmlComment,
+	HtmlEmbed,
+	ImageBlock,
+	ImageCaption,
+	ImageInline,
+	ImageInsert,
+	ImageInsertViaUrl,
+	ImageResize,
+	ImageStyle,
+	ImageTextAlternative,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	IndentBlock,
 	Italic,
 	Link,
+	LinkImage,
+	List,
+	ListProperties,
+	Markdown,
+	MediaEmbed,
 	Paragraph,
+	PasteFromMarkdownExperimental,
+	PasteFromOffice,
+	RemoveFormat,
 	SelectAll,
+	ShowBlocks,
+	SimpleUploadAdapter,
+	SourceEditing,
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Table,
+	TableCaption,
+	TableCellProperties,
+	TableColumnResize,
+	TableProperties,
+	TableToolbar,
+	TextTransformation,
+	TodoList,
+	Underline,
 	Undo
 } from 'ckeditor5';
 
@@ -19,11 +79,189 @@ import en from 'ckeditor5/translations/en.js';
 
 const editorConfig = {
 	toolbar: {
-		items: ['undo', 'redo', '|', 'bold', 'italic', '|', 'link'],
+		items: [
+			'undo',
+			'redo',
+			'|',
+			'sourceEditing',
+			'showBlocks',
+			'|',
+			'heading',
+			'|',
+			'fontSize',
+			'fontFamily',
+			'fontColor',
+			'fontBackgroundColor',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'|',
+			'link',
+			'insertImage',
+			'insertTable',
+			'highlight',
+			'blockQuote',
+			'codeBlock',
+			'|',
+			'alignment',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'todoList',
+			'outdent',
+			'indent'
+		],
 		shouldNotGroupWhenFull: false
 	},
-	plugins: [AccessibilityHelp, AutoLink, Autosave, BalloonToolbar, Bold, Essentials, Italic, Link, Paragraph, SelectAll, Undo],
-	balloonToolbar: ['bold', 'italic', '|', 'link'],
+	plugins: [
+		AccessibilityHelp,
+		Alignment,
+		Autoformat,
+		AutoImage,
+		AutoLink,
+		Autosave,
+		BalloonToolbar,
+		BlockQuote,
+		Bold,
+		Code,
+		CodeBlock,
+		Essentials,
+		FindAndReplace,
+		FontBackgroundColor,
+		FontColor,
+		FontFamily,
+		FontSize,
+		FullPage,
+		GeneralHtmlSupport,
+		Heading,
+		Highlight,
+		HorizontalLine,
+		HtmlComment,
+		HtmlEmbed,
+		ImageBlock,
+		ImageCaption,
+		ImageInline,
+		ImageInsert,
+		ImageInsertViaUrl,
+		ImageResize,
+		ImageStyle,
+		ImageTextAlternative,
+		ImageToolbar,
+		ImageUpload,
+		Indent,
+		IndentBlock,
+		Italic,
+		Link,
+		LinkImage,
+		List,
+		ListProperties,
+		Markdown,
+		MediaEmbed,
+		Paragraph,
+		PasteFromMarkdownExperimental,
+		PasteFromOffice,
+		RemoveFormat,
+		SelectAll,
+		ShowBlocks,
+		SimpleUploadAdapter,
+		SourceEditing,
+		SpecialCharacters,
+		SpecialCharactersArrows,
+		SpecialCharactersCurrency,
+		SpecialCharactersEssentials,
+		SpecialCharactersLatin,
+		SpecialCharactersMathematical,
+		SpecialCharactersText,
+		Strikethrough,
+		Subscript,
+		Superscript,
+		Table,
+		TableCaption,
+		TableCellProperties,
+		TableColumnResize,
+		TableProperties,
+		TableToolbar,
+		TextTransformation,
+		TodoList,
+		Underline,
+		Undo
+	],
+	balloonToolbar: ['bold', 'italic', '|', 'link', 'insertImage', '|', 'bulletedList', 'numberedList'],
+	fontFamily: {
+		supportAllValues: true
+	},
+	fontSize: {
+		options: [10, 12, 14, 'default', 18, 20, 22],
+		supportAllValues: true
+	},
+	heading: {
+		options: [
+			{
+				model: 'paragraph',
+				title: 'Paragraph',
+				class: 'ck-heading_paragraph'
+			},
+			{
+				model: 'heading1',
+				view: 'h1',
+				title: 'Heading 1',
+				class: 'ck-heading_heading1'
+			},
+			{
+				model: 'heading2',
+				view: 'h2',
+				title: 'Heading 2',
+				class: 'ck-heading_heading2'
+			},
+			{
+				model: 'heading3',
+				view: 'h3',
+				title: 'Heading 3',
+				class: 'ck-heading_heading3'
+			},
+			{
+				model: 'heading4',
+				view: 'h4',
+				title: 'Heading 4',
+				class: 'ck-heading_heading4'
+			},
+			{
+				model: 'heading5',
+				view: 'h5',
+				title: 'Heading 5',
+				class: 'ck-heading_heading5'
+			},
+			{
+				model: 'heading6',
+				view: 'h6',
+				title: 'Heading 6',
+				class: 'ck-heading_heading6'
+			}
+		]
+	},
+	htmlSupport: {
+		allow: [
+			{
+				name: /^.*$/,
+				styles: true,
+				attributes: true,
+				classes: true
+			}
+		]
+	},
+	image: {
+		toolbar: [
+			'toggleImageCaption',
+			'imageTextAlternative',
+			'|',
+			'imageStyle:inline',
+			'imageStyle:wrapText',
+			'imageStyle:breakText',
+			'|',
+			'resizeImage'
+		]
+	},
 	language: 'en',
 	link: {
 		addTargetToExternalLinks: true,
@@ -38,10 +276,20 @@ const editorConfig = {
 			}
 		}
 	},
+	list: {
+		properties: {
+			styles: true,
+			startIndex: true,
+			reversed: true
+		}
+	},
 	menuBar: {
 		isVisible: true
 	},
 	placeholder: 'Type or paste your content here!',
+	table: {
+		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+	},
 	translations: [en, es, ca]
 };
 
--- src/Admin/views/attributes/K.blade.php
+++ src/Admin/views/attributes/K.blade.php
@@ -3,14 +3,14 @@
     <div class="column">
         <div class="form-group">
             <label for="{{$attribute_name}}" class="form-label">{{$attribute['caption']}} {!! _attributeInfo($attribute['id'], $attribute['name'], $attribute['type']) !!}</label>
-            <textarea name="{{$attribute_name}}" class="form-control editor" data-language="{{$attribute['language']}}"> disabled>@isset($attribute['atrib_values'][0]){{$attribute['atrib_values'][0]['text_val']}}@endisset</textarea>
+            <textarea name="{{$attribute_name}}" class="form-control editor" data-language="{{$attribute['language']}}">@isset($attribute['atrib_values'][0]){{$attribute['atrib_values'][0]['text_val']}}@endisset</textarea>
         </div>
     </div>
 @elseif($p_mode=='U' || $p_mode=='I')
     <div class="column">
         <div class="form-group">
             <label for="{{$attribute_name}}" class="form-label">{{$attribute['caption']}}</label>
-            <textarea name="{{$attribute_name}}" class="form-control editor" data-language="{{$attribute['language']}}">>@isset($attribute['atrib_values'][0]){{$attribute['atrib_values'][0]['text_val']}}@endisset</textarea>
+            <textarea name="{{$attribute_name}}" class="form-control editor" data-language="{{$attribute['language']}}">@isset($attribute['atrib_values'][0]){{$attribute['atrib_values'][0]['text_val']}}@endisset</textarea>
         </div>
     </div>
 @endif
